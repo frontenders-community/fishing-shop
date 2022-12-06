f55e1ebe87097592daa5d88b4767eb2422cc5fd3
carmhack <carmhack@gmail.com>
2022-12-06 16:03:29 +0100

    complete base version

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
