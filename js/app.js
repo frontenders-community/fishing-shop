c9eff331e1d88dc7ca8d29f3285f46b3ed1770e8
carmhack <carmhack@gmail.com>
2022-12-06 16:18:08 +0100

    add price update and drag and drop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
+let earnings = 0;
+const fishes = [
+    {
+        name: "clown-loach",
+        size: 20,
+        priceByKg: 8,
+    },
+    {
+        name: "seahorse",
+        size: 2,
+        priceByKg: 3.5,
+    },
+    {
+        name: "puffer-fish",
+        size: 8,
+        priceByKg: 6,
+    },
+    {
+        name: "shrimp",
+        size: 6,
+        priceByKg: 14,
+    },
+];
+
+function renderItems() {
+    const itemsContainer = document.querySelector("#items");
+    itemsContainer.innerHTML = "";
+    for(let i = 0; i < fishes.length; i++) {
+        const fish = fishes[i];
+        itemsContainer.innerHTML += `
+            <div class="item">
+                <img id="item-${i}" data-name="${fish.name}" src="assets/fish/${fish.name}.png" alt="${fish.name}" draggable="true" ondragstart="drag(event)">
+            </div>
+        `;
+    }
+}
+
+function renderEarnings() {
+    const showEarningElement = document.querySelector("#earning-value");
+    showEarningElement.innerText = `+${earnings}`;
+}
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -9,5 +51,18 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    const element = document.getElementById(data);
+    ev.target.appendChild(element);
+    updateEarning(element.dataset.name);
 }
+
+function updateEarning(fishName) {
+    const findFish = fishes.find(item => item.name === fishName);
+    if (findFish) {
+        earnings += (findFish.size * findFish.priceByKg);
+        renderEarnings();
+    }
+}
+
+renderItems();
+renderEarnings();
